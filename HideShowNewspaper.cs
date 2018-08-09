2e30b04ff32a2c5456d6c8256ec24b412d7f7072
Keanan Koppenhaver <keanan@howtogeek.com>
2018-08-09 13:01:21 -0500

    Hide table newspaper when closeup newspaper is showing

--- HideShowNewspaper.cs
+++ HideShowNewspaper.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class HideShowNewspaper : MonoBehaviour {
 
-	private GameObject newspaper;
+	private GameObject newspaper_closeup;
+	private GameObject table_newspaper;
 
 	// Use this for initialization
 	void Start () {
-		newspaper = GameObject.Find("NewspaperCanvas");
-		newspaper.SetActive(false);
+		newspaper_closeup = GameObject.Find("NewspaperCanvas");
+		table_newspaper = GameObject.Find("newspaper");
+		newspaper_closeup.SetActive(false);
 	}
 	
 	// Update is called once per frame
@@ -17,11 +19,13 @@ public class HideShowNewspaper : MonoBehaviour {
 		OVRInput.Controller activeController = OVRInput.GetActiveController();
 
 		if( OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger) ) {
-			if (newspaper.activeSelf) {
-      			newspaper.SetActive(false);
+			if (newspaper_closeup.activeSelf) {
+      			newspaper_closeup.SetActive(false);
+      			table_newspaper.SetActive(true);
  			}
  			else {
- 				newspaper.SetActive(true);
+ 				newspaper_closeup.SetActive(true);
+ 				table_newspaper.SetActive(false);
  			}
 		}
 	}
